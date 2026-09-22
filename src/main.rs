a7f00bf9722f86e9c700c98e4c10a82493a590e5
Prasiddha Paudyal <ppaudya1@students.towson.edu>
2026-09-21 23:49:45 -0400

    added array and declare variable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 fn main() {
     println!("Hello, world!");
+    decl_var();
+    array();
 }
 //testing comment
+
+fn decl_var(){
+    let x = 10;
+    println!("x is {}", x);
+}
+
+fn array(){
+    let mut letters = ['a', 'b', 'c'];
+    letters[0] = 'x';
+    let first_letter = letters[0];
+    println!("first_letter is {}", first_letter);
+
+    let numbers: [i32; 5];
+    numbers = [0; 5];
+    let index: usize = numbers.len();
+    println!("last number is {}", numbers[index - 1]);
+}
